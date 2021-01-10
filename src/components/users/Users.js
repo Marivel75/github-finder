694cd1a93cba050109dc76e370350c9646dd6514
Mika <>
2021-01-10 22:40:31 +0100

    first commit at last

---
+++ src/components/users/Users.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import UserItem from './UserItem';
+import Spinner from '../layouts/Spinner';
+import PropTypes from 'prop-types';
+
+
+const Users = ({users, loading}) => {
+    if (loading) {
+        return <div className="container" style={{textAlign: 'center'}}>
+            <Spinner />
+        </div>
+    } else {
+        return (
+            <div style={userStyle}>
+                {users.map(user => (
+                    <UserItem key={user.id} user={user} />
+                ))}
+            </div>
+        );
+    }
+}
+
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired,
+}
+
+
+const userStyle = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gridGap: '1rem'
+}
+
+export default Users
