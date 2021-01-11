63e24ac11343cc66566a7d9be572289c1adcf052
Mika <>
2021-01-11 20:47:25 +0100

    refactored Search component into function based

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -67,7 +67,7 @@ export class User extends Component {
                             <p>{bio}</p>
                         </Fragment>
                     }
-                    <a href={html_url} className="btn btn-dark my-1" target="_blank">
+                    <a href={html_url} className="btn btn-dark my-1">
                         Visit Github Profile
                     </a>
                     <ul>
